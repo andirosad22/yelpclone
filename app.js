8c0be3d84c4a2106096e4e56a71481842949e299
andirosad22 <andirosad22@gmail.com>
2023-12-04 07:56:54 +0700

    seed: create seed in app.js /seed/place

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+
+// Models
+const Place = require('./models/place');
+
+mongoose.connect('mongodb://127.0.0.1/bestpoints')
+.then((result)=>{
+    console.log('connected to mongoose');
+}).catch((err)=>{
+    console.log(err);
+});
 
 // iniciete ejs
 app.set('view engine', 'ejs');
@@ -10,6 +21,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.render('home');
 })
+
+app.get('/seed/place', async(req, res) => {
+  const place = new Place({
+    title: 'Empire State Building',
+    price: '$999',
+    descriptionn: 'A great building',
+    location: 'New York, NY'
+  });
+  await place.save();
+  res.send(place);
+})
+
 app.listen(8080, () => {
   console.log(`server is running on http://127.0.0.1:8080`);
 })
---
+++ models/place.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  title: String,
+  price: String,
+  descriptionn: String,
+  location: String
+});
+
+const Place = mongoose.model('Place', placeSchema);
+module.exports = Place;
