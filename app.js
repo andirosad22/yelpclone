a1163d743a7ff9f81c6c15b2452e2e51cb3bc5f5
andirosad22 <andirosad22@gmail.com>
2023-12-04 09:05:11 +0700

    Fitures: create data place and save to database

--- app.js
+++ app.js
@@ -17,20 +17,31 @@ mongoose.connect('mongodb://127.0.0.1/bestpoints')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.render('home');
 })
 
-app.get('/place', async(req, res) => {
+app.get('/places', async(req, res) => {
   const places = await Place.find();
   res.render('places/index', {places});
 });
-app.get('/place/:id', async (req, res) => {
-  // const {id} = req.params;
+
+app.get('/place/create', (req, res) => {
+  res.render('places/create');
+});
+app.post('/places', async(req, res) => {
+  const place = new Place(req.body.place);
+  await place.save();
+  res.redirect('/places');
+})
+app.get('/place/:id', async (req, res) => {  
   const place = await Place.findById(req.params.id);
   res.render('places/show', {place});
-})
+});
 
 app.listen(8080, () => {
   console.log(`server is running on http://127.0.0.1:8080`);
